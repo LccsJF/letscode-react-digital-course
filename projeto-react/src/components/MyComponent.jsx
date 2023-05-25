c7c8a25e22c1e810e83bd076394fdedca5db8de4
Bowie <marketing@letscode.com.br>
2023-05-25 17:18:45 -0300

    criando componente MyComponent

---
+++ projeto-react/src/components/MyComponent.jsx
@@ -0,0 +1,10 @@
+export default function MyComponent() {
+  return (
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores optio
+      sed quasi, fuga fugiat veniam labore excepturi. Consequuntur nihil id
+      molestias, tempora eligendi accusamus vero similique excepturi libero
+      maiores voluptas!
+    </p>
+  );
+}
